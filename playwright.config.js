020366f2eab068ca00e2ca56715ba6330408229a
Siva Prakash <psivaprakash38@gmail.com>
2026-06-10 10:46:01 +0530

    updated configs

--- playwright.config.js
+++ playwright.config.js
@@ -30,6 +30,7 @@ export default defineConfig({
     // baseURL: 'http://localhost:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+    baseURL: "http://localhost:8888",
     trace: 'on-first-retry',
     video: 'retain-on-failure'
   },
@@ -73,10 +74,10 @@ export default defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://localhost:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
+  webServer: {
+    command: 'npm run start',
+    url: 'http://localhost:8888',
+    reuseExistingServer: !process.env.CI,
+  },
 });
 
