b61caf038304f16b4bf566f24187a51afb718e91
Siva Prakash <psivaprakash38@gmail.com>
2026-06-10 10:59:38 +0530

    updated config

--- playwright.config.js
+++ playwright.config.js
@@ -74,10 +74,10 @@ export default defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://localhost:8888',
-  //   reuseExistingServer: !process.env.CI,
-  // },
+  webServer: {
+    command: 'npm run start',
+    url: 'http://localhost:8888',
+    reuseExistingServer: !process.env.CI,
+  },
 });
 
